a67fb60c0e48829c848c006b1833832f9d98f46d
조수훈 <>
2024-06-01 18:45:51 +0900

    [feat] add dynamic matadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import MainHeader from '@/components/main-header/main-header';
 import './globals.css';
 
+// 페이지에 메타데이터가 있으면, 페이지 메타데이터가 우선 적용됨.
+// 중첩된 레이아웃에 메타데이터가 있으면, 레이아웃 메타데이터가 우선 적용됨.
 export const metadata = {
   title: 'NextLevel Food',
   description: 'Delicious meals, shared by a food-loving community.',
--- src/app/meals/[mealsSlug]/page.jsx
+++ src/app/meals/[mealsSlug]/page.jsx
@@ -4,8 +4,27 @@ import { notFound } from 'next/navigation';
 import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 
+/**
+ * 동적으로 메타데이터 추가하기.
+ * Next js가 `generateMetadata`의 이름을 가진 함수를 찾기 때문에
+ * 함수명은 항상 generateMetadata로 고정.
+ * 
+ * @returns {} metadata 객체
+ */
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealsSlug);
+  
+  if (!meal) {
+    notFound();
+  }
+  
+  return {
+    title: meal.title,
+    description: meal.summary
+  }
+}
+
 export default function MealDetailsPage({ params }) {
-  console.log(params)
   const meal = getMeal(params.mealsSlug);
   if (!meal) {
     notFound();
